e02f879403685120853a811dabb06c85ca6946e2
Bearfighter19 <104339146+Bearfighter19@users.noreply.github.com>
2026-04-29 23:10:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const Gamedig = require('gamedig');
+const config = require('./config.json');
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+const TOKEN = process.env.TOKEN;
+
+if (!TOKEN) {
+    console.error("Missing TOKEN");
+    process.exit(1);
+}
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+
+    setInterval(async () => {
+        try {
+            const state = await Gamedig.query({
+                type: 'dayz',
+                host: config.server.ip,
+                port: config.server.queryPort
+            });
+
+            client.user.setPresence({
+                activities: [{
+                    name: `DayZ: ${state.players.length}/${state.maxplayers}`,
+                    type: ActivityType.Playing
+                }],
+                status: 'online'
+            });
+
+        } catch {
+            client.user.setPresence({
+                activities: [{
+                    name: `DayZ: Offline`,
+                    type: ActivityType.Playing
+                }],
+                status: 'dnd'
+            });
+        }
+    }, 60000);
+});
+
+client.login(TOKEN);
