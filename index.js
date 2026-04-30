ebec972b99f4f9c10464ad8ece8f1c17022d6170
Bearfighter19 <104339146+Bearfighter19@users.noreply.github.com>
2026-04-30 01:11:58 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
-const Gamedig = require('gamedig');
-const config = require('./config.json');
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
+const Gamedig = require("gamedig");
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
 const TOKEN = process.env.TOKEN;
+const HOST = process.env.HOST;
+const PORT = process.env.PORT || 2302;
+const INTERVAL = process.env.INTERVAL || 60000;
 
-if (!TOKEN) {
-    console.error("Missing TOKEN");
-    process.exit(1);
+async function updateServerStatus() {
+    try {
+        const state = await Gamedig.query({
+            type: "dayz",
+            host: HOST,
+            port: PORT
+        });
+
+        const players = state.players.length;
+        const maxPlayers = state.maxplayers;
+
+        client.user.setActivity(
+            `${players}/${maxPlayers} Players Online`,
+            { type: ActivityType.Watching }
+        );
+    } catch (err) {
+        client.user.setActivity("Server Offline", {
+            type: ActivityType.Watching
+        });
+    }
 }
 
-client.once('ready', () => {
+client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
 
-    setInterval(async () => {
-        try {
-            const state = await Gamedig.query({
-                type: 'dayz',
-                host: config.server.ip,
-                port: config.server.queryPort
-            });
-
-            client.user.setPresence({
-                activities: [{
-                    name: `DayZ: ${state.players.length}/${state.maxplayers}`,
-                    type: ActivityType.Playing
-                }],
-                status: 'online'
-            });
-
-        } catch {
-            client.user.setPresence({
-                activities: [{
-                    name: `DayZ: Offline`,
-                    type: ActivityType.Playing
-                }],
-                status: 'dnd'
-            });
-        }
-    }, 60000);
+    updateServerStatus();
+    setInterval(updateServerStatus, INTERVAL);
 });
 
 client.login(TOKEN);
